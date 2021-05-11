60c55419e50a968665434dfd46e816da2a3f3283
catchpwb <63562752+catchpwb@users.noreply.github.com>
2021-05-10 20:34:25 -0400

    Add files via upload

---
+++ Four.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Four {
+    /*
+    Calculates the volume v of a cube with dimensions l, b, and h, all floating point
+     */
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in, "UTF-8");
+
+        System.out.println("Enter the length, breadth, and height of the cube [float float float]: ");
+        float l = keyboard.nextInt();
+        float b = keyboard.nextInt();
+        float h = keyboard.nextInt();
+        System.out.printf("The volume of the cube is: %f", l * b * h);
+
+    }
+}
