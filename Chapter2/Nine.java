9f07bd260c2b5859ebdab0a839c23b3732a12804
catchpwb <63562752+catchpwb@users.noreply.github.com>
2021-05-10 22:34:58 -0400

    Add files via upload

--- Chapter2/Nine.java
+++ Chapter2/Nine.java
@@ -2,7 +2,7 @@ package Chapter2;
 
 import java.util.Scanner;
 
-public class Five {
+public class Nine {
 
     /*
     Calculates the area a and circumference c of a circle
@@ -10,12 +10,14 @@ public class Five {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in, "UTF-8");
 
+        System.out.println("Please enter the units used to measure the radius: ");
+        String units = keyboard.nextLine();
         System.out.println("Please enter the radius of the circle: ");
-        int r = keyboard.nextInt();
+        double r = keyboard.nextDouble();
         //Specify the value of pi to be unchanging
         final double p = 3.1415927;
-        System.out.printf("The area of the cirle is: %f %n", p * r * r);
-        System.out.printf("The circumference of the circle is: %f %n", 2 * p * r);
+        System.out.printf("The area of the cirle is: %f %s %n", p * r * r, units);
+        System.out.printf("The circumference of the circle is: %f %s %n", 2 * p * r, units);
 
 
     }
